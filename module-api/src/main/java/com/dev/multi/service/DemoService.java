edd20cbee9d24b439293e636943f5fab79249ac8
LimJiNa <jn4624@gmail.com>
2023-08-31 00:56:26 +0900

    Bean을 사용한 모듈간 의존성 테스트 - close #6

--- module-api/src/main/java/com/dev/multi/service/DemoService.java
+++ module-api/src/main/java/com/dev/multi/service/DemoService.java
@@ -1,16 +1,21 @@
 package com.dev.multi.service;
 
 import com.dev.multi.enums.CodeEnum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DemoService {
+    private final CommonDemoService commonDemoService;
+
     public String save() {
         System.out.println(CodeEnum.SUCCESS.getCode());
         return "save";
     }
 
     public String find() {
+        System.out.println(commonDemoService.commonService());
         return "find";
     }
 }
